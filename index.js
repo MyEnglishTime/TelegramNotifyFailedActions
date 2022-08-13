cd117b0a4259bc9a865d186cebaa074c2290dad0
Skwal98 <levkalex98@gmail.com>
2022-08-14 00:05:54 +0300

    s

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
+var core = __importStar(require("@actions/core"));
+var github = __importStar(require("@actions/github"));
 var world = "world";
-function hello(who) {
-    if (who === void 0) { who = world; }
-    return "Hello " + who + "! ";
-}
-exports.hello = hello;
-console.log(hello());
+var payload = github.context.payload;
+var actor = github.context.actor;
+var eventName = github.context.eventName;
+console.log(payload);
+console.log(actor);
+console.log(eventName);
+console.log(core.getInput("status"));
--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
 const world = "world";
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
-}
+const payload = github.context.payload;
+const actor = github.context.actor;
+const eventName = github.context.eventName;
 
-console.log(hello());
+console.log(payload);
+console.log(actor);
+console.log(eventName);
+
+console.log(core.getInput("status"));
